cd0a09a6c4456363f8366a7166944f68db9af801
Henrique <Henrique@DESKTOP-32T0UMT>
2023-02-13 20:14:50 +0000

    Adicionar arquivos de projeto.

---
+++ B1n4r10/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace B1n4r10
+{
+    partial class B1n4r10
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(B1n4r10));
+            this.txtInput = new System.Windows.Forms.RichTextBox();
+            this.txtOutput = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnTranslate = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.rbnTextToBinary = new System.Windows.Forms.RadioButton();
+            this.rbnBinaryToText = new System.Windows.Forms.RadioButton();
+            this.btnPaste = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtInput.Location = new System.Drawing.Point(28, 27);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(286, 483);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.Text = "";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtOutput.Location = new System.Drawing.Point(353, 27);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(286, 483);
+            this.txtOutput.TabIndex = 1;
+            this.txtOutput.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Input";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(350, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Output";
+            // 
+            // btnTranslate
+            // 
+            this.btnTranslate.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnTranslate.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnTranslate.Location = new System.Drawing.Point(773, 208);
+            this.btnTranslate.Name = "btnTranslate";
+            this.btnTranslate.Size = new System.Drawing.Size(138, 23);
+            this.btnTranslate.TabIndex = 4;
+            this.btnTranslate.Text = "Traduzir";
+            this.btnTranslate.UseVisualStyleBackColor = false;
+            this.btnTranslate.Click += new System.EventHandler(this.btnTranslate_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnCopy.Location = new System.Drawing.Point(773, 237);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(138, 23);
+            this.btnCopy.TabIndex = 5;
+            this.btnCopy.Text = "Copiar";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // rbnTextToBinary
+            // 
+            this.rbnTextToBinary.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.rbnTextToBinary.AutoSize = true;
+            this.rbnTextToBinary.Checked = true;
+            this.rbnTextToBinary.Location = new System.Drawing.Point(773, 82);
+            this.rbnTextToBinary.Name = "rbnTextToBinary";
+            this.rbnTextToBinary.Size = new System.Drawing.Size(138, 20);
+            this.rbnTextToBinary.TabIndex = 6;
+            this.rbnTextToBinary.TabStop = true;
+            this.rbnTextToBinary.Text = "Texto para Binário";
+            this.rbnTextToBinary.UseVisualStyleBackColor = true;
+            // 
+            // rbnBinaryToText
+            // 
+            this.rbnBinaryToText.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.rbnBinaryToText.AutoSize = true;
+            this.rbnBinaryToText.Location = new System.Drawing.Point(773, 130);
+            this.rbnBinaryToText.Name = "rbnBinaryToText";
+            this.rbnBinaryToText.Size = new System.Drawing.Size(138, 20);
+            this.rbnBinaryToText.TabIndex = 7;
+            this.rbnBinaryToText.Text = "Binário para Texto";
+            this.rbnBinaryToText.UseVisualStyleBackColor = true;
+            // 
+            // btnPaste
+            // 
+            this.btnPaste.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnPaste.Location = new System.Drawing.Point(773, 266);
+            this.btnPaste.Name = "btnPaste";
+            this.btnPaste.Size = new System.Drawing.Size(138, 23);
+            this.btnPaste.TabIndex = 8;
+            this.btnPaste.Text = "Colar";
+            this.btnPaste.UseVisualStyleBackColor = true;
+            this.btnPaste.Click += new System.EventHandler(this.btnPaste_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(812, 295);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(62, 23);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "Limpar";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnHelp.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHelp.Location = new System.Drawing.Point(812, 487);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(62, 23);
+            this.btnHelp.TabIndex = 10;
+            this.btnHelp.Text = "Ajuda";
+            this.btnHelp.UseVisualStyleBackColor = true;
+            this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
+            // 
+            // B1n4r10
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1024, 527);
+            this.Controls.Add(this.btnHelp);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnPaste);
+            this.Controls.Add(this.rbnBinaryToText);
+            this.Controls.Add(this.rbnTextToBinary);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnTranslate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "B1n4r10";
+            this.Text = "B1n4r10";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtInput;
+        private System.Windows.Forms.RichTextBox txtOutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnTranslate;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.RadioButton rbnTextToBinary;
+        private System.Windows.Forms.RadioButton rbnBinaryToText;
+        private System.Windows.Forms.Button btnPaste;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnHelp;
+    }
+}
+
---
+++ B1n4r10/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace B1n4r10
+{
+    public partial class B1n4r10 : Form
+    {
+    Dictionary<char, string> binaryCode = new Dictionary<char, string>
+    {
+        {'A', "01000001"}, 
+        {'B', "01000010"}, 
+        {'C', "01000011"},
+        {'D', "01000100"}, 
+        {'E', "01000101"}, 
+        {'F', "01000110"},
+        {'G', "01000111"}, 
+        {'H', "01001000"}, 
+        {'I', "01001001"},
+        {'J', "01001010"}, 
+        {'K', "01001011"}, 
+        {'L', "01001100"},
+        {'M', "01001101"}, 
+        {'N', "01001110"}, 
+        {'O', "01001111"},
+        {'P', "01010000"}, 
+        {'Q', "01010001"}, 
+        {'R', "01010010"},
+        {'S', "01010011"}, 
+        {'T', "01010100"}, 
+        {'U', "01010101"},
+        {'V', "01010110"}, 
+        {'W', "01010111"}, 
+        {'X', "01011000"},
+        {'Y', "01011001"},
+        {'Z', "01011010"},
+        {'0', "00110000"},
+        {'1', "00110001"}, 
+        {'2', "00110010"},
+        {'3', "00110011"}, 
+        {'4', "00110100"}, 
+        {'5', "00110101"},
+        {'6', "00110110"}, 
+        {'7', "00110111"}, 
+        {'8', "00111000"},
+        {'9', "00111001"},
+        {' ', "00100000"},
+        {'!', "00100001"},
+        {'"', "00100010"},
+        {'#', "00100011"},
+        {'$', "00100100"},
+        {'%', "00100101"},
+        {'&', "00100110"},
+        {'(', "00101000"},
+        {')', "00101001"},
+        {'*', "00101010"},
+        {',', "00101100"},
+        {'-', "00101101"},
+        {'.', "00101110"},
+        {'/', "00101111"},
+        {':', "00111010"},
+        {'<', "00111100"},
+        {'=', "00111101"},
+        {'>', "00111110"},
+        {'?', "00111111"},
+        {'@', "01000000"},
+        {'_', "01011111"},
+        {'´', "11000010 10110100"},
+        {'`', "01100000"},
+        {'[', "01011011"},
+        {']', "01011101"},
+        {'^', "01011110"},
+        {'{', "01111011"},
+        {'|', "01111100"},
+        {'}', "01111101"},
+        {'~', "01111110"},
+        //Portuguese Characters
+        {'Ç', "10000111"},
+        {'Á', "10000001"},
+        {'Ã', "10000011"},
+        {'À', "10000000"},
+        {'Â', "10000010"},
+        {'É', "10001001"},
+        {'È', "10001000"},
+        {'Ê', "10001010"},
+        {'Ì', "10001100"},
+        {'Î', "10001110"},
+        {'Í', "10001101"},
+        {'Ó', "10010011"},
+        {'Õ', "10010101"},
+        {'Ò', "10010010"},
+        {'Ô', "10010100"},
+        {'Ú', "10011010"},
+        {'Û', "10011011"},
+        {'Ù', "10011001"}
+
+    };
+
+        private Dictionary<string, char> ReverseBinaryCode { get; set; }
+        public B1n4r10()
+        {
+            InitializeComponent();
+            ReverseBinaryCode = binaryCode.ToDictionary(x => x.Value, x => x.Key);
+
+        }
+
+        private void btnTranslate_Click(object sender, EventArgs e)
+        {
+            string input = txtInput.Text.ToUpper();
+            string output = "";
+
+            if (rbnTextToBinary.Checked == true)
+            {
+                foreach (char letter in input)
+                {
+                    if (binaryCode.ContainsKey(letter))
+                    {
+                        output += binaryCode[letter] + " ";
+                    }
+                }
+            }
+            else if (rbnBinaryToText.Checked == true)
+            {
+                string[] binaryStrings = input.Split(' ');
+                foreach (string binaryString in binaryStrings)
+                {
+                    if (ReverseBinaryCode.ContainsKey(binaryString))
+                    {
+                        output += ReverseBinaryCode[binaryString];
+                    }
+                }
+            }
+
+            
+            txtOutput.Text = output;
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            if (txtOutput.Text == "")
+            {
+                MessageBox.Show("Para copiar o Output, é preciso haver Output." +
+                    "\n\nPeço-te se não for muito maçante, escreve alguma coisa no Input.", "Sem Output", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                Clipboard.SetText(txtOutput.Text);
+                MessageBox.Show("Copiado com sucesso!", "Copiado!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnPaste_Click(object sender, EventArgs e)
+        {
+            txtInput.Text = Clipboard.GetText();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtInput.Text = "";
+            txtOutput.Text = "";
+        }
+
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+            string message = "Eu ao contrário do meu projeto de código morse, consegui colocar caracteres acentuados usados na linguagem portuguesa. Mas sim pode haver uma fuga, por favor se encontrar alçgum erro, dirija-se ao meu Website e reporte o problema.";
+            string ws = Environment.NewLine + "\nhttps://h3nr1qu3x.github.io/website/contact.html";
+            MessageBox.Show(message + ws, "Ajuda", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
